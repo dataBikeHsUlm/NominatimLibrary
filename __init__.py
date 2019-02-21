83ae31f615af4213cfd97c72ffd38120f21dfd07
GONI Guillaume <gdn3@live.fr>
2019-02-21 08:15:01 +0100

    moved get_route to method

--- __init__.py
+++ __init__.py
@@ -20,39 +20,6 @@ class NotFoundException(Exception):
 def format_points_graphhopper(point):
     return "&point=" + str(point[0]) + "," + str(point[1])
 
-def get_route(coord0, coord1, calc_points=False):
-    """
-    Gets the coordinates to a given address
-
-    # See https://graphhopper.com/api/1/docs/routing/
-
-    Returns:
-        Dictionnary containing the shortest path information :
-        path = {
-                "distance": distance in meters
-                "time": time in milisecond
-                "bbox": coordinates of the rectangle containing the path : [minLon, minLat, maxLon, maxLat]
-                "points": array of coordinates arrays : [[lon,lat]] only if `calc_points` is True
-        }
-    """
-
-    calc_points_str = "&calc_points=" + str(calc_points)
-    points_str = format_points_graphhopper(coord0) + format_points_graphhopper(coord1)
-    query = GRAPHHOPPER_API + "/" + GRAPHHOPPER_ROUTE_QUERY + calc_points_str + points_str
-
-    answer = urllib.request.urlopen(query).read()
-    parsed_json = json.loads(answer)
-    first_path = parsed_json["paths"][0]
-
-    path = {
-            "distance": first_path["distance"],
-            "time": first_path["time"],
-            "bbox": first_path["bbox"],
-            "points": first_path["points"]["coordinates"]
-    }
-
-    return path
-
 class Locator:
     def __init__(self, domain = DOMAIN):
         geopy.geocoders.options.default_scheme = "http"
@@ -120,6 +87,62 @@ class Locator:
         return distance.distance(coord0, coord1).km
         # return distance.geodesic(coord0,coord1, ellipsoid='GRS-80').km
 
+    def path_route_addrs(self, address0, address1, calc_points=True):
+        """
+        Gets the distance on the route between two addresses
+
+        Args:
+            address0 and address1 (str) : String List of following arguments: house_number,road, town, city, county, state_district, state, postcode, country, country_code
+
+        Returns:
+            Dictionnary containing the shortest path information :
+            path = {
+                    "distance": distance in meters
+                    "time": time in milisecond
+                    "bbox": coordinates of the rectangle containing the path : [minLon, minLat, maxLon, maxLat]
+                    "points": array of coordinates arrays : [[lon,lat]] only if `calc_points` is True
+            }
+        """
+
+        coord0 = self.get_coordinates(address0)
+        coord1 = self.get_coordinates(address1)
+        return self.path_route_coords(coord0, coord1, calc_points)
+
+    def path_route_coords(self, coord0, coord1, calc_points=True):
+        """
+        Gets the coordinates to a given address
+
+        # See https://graphhopper.com/api/1/docs/routing/
+
+        Returns:
+            Dictionnary containing the shortest path information :
+            path = {
+                    "distance": distance in meters
+                    "time": time in milisecond
+                    "bbox": coordinates of the rectangle containing the path : [minLon, minLat, maxLon, maxLat]
+                    "points": array of coordinates arrays : [[lon,lat]] only if `calc_points` is True
+            }
+        """
+
+        calc_points_str = "&calc_points=" + str(calc_points)
+        points_str = format_points_graphhopper(coord0) + format_points_graphhopper(coord1)
+        query = GRAPHHOPPER_API + "/" + GRAPHHOPPER_ROUTE_QUERY + calc_points_str + points_str
+
+        answer = urllib.request.urlopen(query).read()
+        parsed_json = json.loads(answer)
+        first_path = parsed_json["paths"][0]
+
+        path = {
+                "distance": first_path["distance"],
+                "time": first_path["time"],
+        }
+        
+        if calc_points:
+            path["bbox"] = first_path["bbox"]
+            path["points"] = first_path["points"]["coordinates"]
+
+        return path
+
     def distance_route_addrs(self, address0, address1):
         """Gets the distance on the route between two addresses
 
@@ -129,9 +152,7 @@ class Locator:
             Returns:
                 float : distance in km
         """
-        coord0 = self.get_coordinates(address0)
-        coord1 = self.get_coordinates(address1)
-        return self.distance_route_coords(coord0, coord1)
+        return self.path_route_addrs(address0, address1, False)["distance"] / 1000
 
     def distance_route_coords(self, coord0, coord1):
         """Gets the distance on the route between two addresses
@@ -143,9 +164,7 @@ class Locator:
                 float : distance in km
         """
 
-        route = get_route(coord0, coord1, False)
-
-        return ["distance"] / 1000
+        return self.path_route_coords(coord0, coord1, False)["distance"] / 1000
 
 if __name__ == "__main__":
     ltr = Locator()
